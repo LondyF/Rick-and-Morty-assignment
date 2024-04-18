19089599cee935759e877f0ba2cd3686db33a8fe
Londy Ferero <londy@iodigital.com>
2024-04-18 18:51:45 +0200

    fix(filters): Clear name input when name filter changes

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,9 +3,18 @@ import { TextField } from "@mui/material";
 import { useFilters } from "../../contexts/filters";
 import { useDebounce } from "../../hooks/use-debounce";
 import { DEBOUNCE_DELAY } from "../../config";
+import React from "react";
 
 const SearchBar = () => {
-  const { updateFilters } = useFilters();
+  const { updateFilters, filters } = useFilters();
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    if (!filters.name) {
+      inputRef.current!.value = "";
+    }
+  }, [filters.name]);
 
   const handleNameFilterChange = useDebounce(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,6 +25,7 @@ const SearchBar = () => {
 
   return (
     <TextField
+      inputRef={inputRef}
       onChange={handleNameFilterChange}
       fullWidth
       type="text"
